6e3cfbca83615368b5079c3e114508a10e7863dd
RobloxAvatar <85132986+RobloxAvatar@users.noreply.github.com>
2021-12-24 20:22:52 +0100

    Update Main.lua

--- Main.lua
+++ Main.lua
@@ -1,6 +1,6 @@
 local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHeptc/Kavo-UI-Library/main/source.lua"))()
 
-local Window = Library.CreateLib("Brookhaven", "Serpent")
+local Window = Library.CreateLib("Jailbreak", "Serpent")
 
 local MainTab = Window:NewTab("Main")
 
