b34cc0b5756819f28e8c73210291c0eb605d4cf7
RobloxAvatar <85132986+RobloxAvatar@users.noreply.github.com>
2021-12-24 20:05:15 +0100

    Update Main.lua

--- Main.lua
+++ Main.lua
@@ -1,6 +1,6 @@
 local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHeptc/Kavo-UI-Library/main/source.lua"))()
 
-local Window = Library.CreateLib("Jailbreak", "Serpent")
+local Window = Library.CreateLib("Brookhaven", "Serpent")
 
 local MainTab = Window:NewTab("Main")
 
@@ -10,21 +10,13 @@ MainSection:NewButton("Drop Esp", "gives you drop esp!", function()
     loadstring(game:HttpGet("https://raw.githubusercontent.com/RobloxAvatar/Jailbreak/main/Drop-Esp.lua", true))()
 end)
 
-MainSection:NewToggle("Inf Nitro", "gives you infinite nitro!", function(state)
-    if state then
-        while true do wait()
-            for i,v in next, getgc(true) do
-                if type(v) == "table" and rawget(v, "Nitro") then
-                    v.Nitro = math.huge
-                    v.NitroLastMax = math.huge
-                end
-            end
-        end
-    else
+game:GetService("UserInputService").InputBegan:Connect(function(i, typing)
+    if typing then return end
+    if i.KeyCode == Enum.KeyCode.Q then
         for i,v in next, getgc(true) do
             if type(v) == "table" and rawget(v, "Nitro") then
-                v.Nitro = 0
-                v.NitroLastMax = 100
+                v.Nitro = math.huge
+                v.NitroLastMax = math.huge
             end
         end
     end
